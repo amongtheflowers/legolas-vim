eac91c910beeb45e213834180d033b70c3650714
花毒 <cihuajian@gmail.com>
2020-02-16 17:35:41 +0800

    Update init-vim-centos.sh

--- scripts/init-vim-centos.sh
+++ scripts/init-vim-centos.sh
@@ -10,4 +10,6 @@ yum install -y gcc gcc-c++ kernel-devel cmake golang python-pip python-devel pyt
 
 sudo ln -s /usr/bin/python3.6 /usr/bin/python3
 
-yum remove vim-* -y \ && wget -P /etc/yum.repos.d/ https://copr.fedorainfracloud.org/coprs/lbiaggi/vim80-ligatures/repo/epel-7/lbiaggi-vim80-ligatures-epel-7.repo \ && yum install vim-enhanced sudo -y
+sudo yum remove vim-* -y
+wget -P /etc/yum.repos.d/ https://copr.fedorainfracloud.org/coprs/lbiaggi/vim80-ligatures/repo/epel-7/lbiaggi-vim80-ligatures-epel-7.repo
+sudo yum install vim-enhanced
